8fd51e40518a95b8df395bcf0b2da65153077baf
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2025-03-02 20:10:19 -0300

    Initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,94 @@
+﻿using CleanArchitecture.Application.UseCases.CreateUser;
+using CleanArchitecture.Application.UseCases.DeleteUser;
+using CleanArchitecture.Application.UseCases.GetAllUser;
+using CleanArchitecture.Application.UseCases.UpdateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        /// <summary>
+        /// Retrieves all users.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>List of all users.</returns>
+        [HttpGet]
+        public async Task<ActionResult<List<GetAllUserResponse>>> GetAll(CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new GetAllUserRequest(), cancellationToken);
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Creates a new user.
+        /// </summary>
+        /// <param name="request">Request containing user details.</param>
+        /// <returns>Id of the created user.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserRequest request)
+        {
+            if (request == null)
+            {
+                return BadRequest("Request cannot be null.");
+            }
+
+            var userId = await _mediator.Send(request);
+            return Ok(userId);
+        }
+
+        /// <summary>
+        /// Updates an existing user.
+        /// </summary>
+        /// <param name="id">User ID to update.</param>
+        /// <param name="request">Request containing updated user details.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Updated user details.</returns>
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UpdateUserResponse>> Update(Guid id, [FromBody] UpdateUserRequest request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                return BadRequest("Request cannot be null.");
+            }
+
+            if (id != request.Id)
+            {
+                return BadRequest("The ID in the URL does not match the ID in the request body.");
+            }
+
+            var response = await _mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Deletes an existing user.
+        /// </summary>
+        /// <param name="id">User ID to delete.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Deletion result.</returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid? id, CancellationToken cancellationToken)
+        {
+            if (id == null)
+            {
+                return BadRequest("User ID cannot be null.");
+            }
+
+            var deleteUserRequest = new DeleteUserRequest(id.Value);
+
+            var response = await _mediator.Send(deleteUserRequest, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
---
+++ Extensions/CorsPolicyExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace CleanArchitecture.WebAPI.Extensions
+{
+    public static class CorsPolicyExtensions
+    {
+        /// <summary>
+        /// Configures the default CORS policy to allow any origin, method, and header.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to add the CORS policy to.</param>
+        public static void ConfigureCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+        }
+
+        /// <summary>
+        /// Configures a named CORS policy with specified options.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to add the CORS policy to.</param>
+        /// <param name="policyName">The name of the CORS policy.</param>
+        /// <param name="configurePolicy">A delegate to configure the policy.</param>
+        public static void ConfigureNamedCorsPolicy(this IServiceCollection services, string policyName, Action<CorsPolicyBuilder> configurePolicy)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(policyName, configurePolicy);
+            });
+        }
+    }
+}
